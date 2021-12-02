a84523c18d45f18a188e56e0464157fbee4084ba
Safayet Hossai <safayet.hossain@bs-23.com>
2021-12-02 14:40:09 +0600

    crud done

--- WebAPI/UI/my-app/src/Department.js
+++ WebAPI/UI/my-app/src/Department.js
@@ -28,11 +28,13 @@ export class Department extends Component{
     changeDepartmentName=(e)=>{
         this.setState({DepartmentName:e.target.value});
     }
+   
     addClick(){
+        debugger
         this.setState({
+            
             modalTitle:"Add Department",
-            DepartmentId:0,
-            DepartmentName:""
+            
         });
     }
     editClick(dep){
@@ -41,6 +43,64 @@ export class Department extends Component{
             DepartmentId:dep.DepartmentId,
             DepartmentName:dep.DepartmentName
         });
+    }
+    createClick(){
+        fetch(variables.API_URL+'department',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                DepartmentName:this.state.DepartmentName
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+    updateClick(){
+        fetch(variables.API_URL+'department',{
+            method:'PUT',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                DepartmentId:this.state.DepartmentId,
+                DepartmentName:this.state.DepartmentName
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    deleteClick(id){
+        if(window.confirm('Are you sure?')){
+        fetch(variables.API_URL+'department/'+id,{
+            method:'DELETE',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            }
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+        }
     }
     render(){
         const {
@@ -85,10 +145,10 @@ export class Department extends Component{
                                 </button>
                                  
                                 <button type="button"
-                                className="btn btn-light mr-1">
-                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                className="btn btn-light mr-1"
+                                onClick={()=>this.deleteClick(dep.DepartmentId)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                     </svg>
                                 </button>
                             </td>
@@ -104,14 +164,27 @@ export class Department extends Component{
                     <h5 className="modal-title">{modalTitle}</h5> 
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                        <div className="modal-body">
+                        <div className="modal-body">                            
                             <div className="input-group mb-3">
                                 <span className="input-group-text">DepartmentName</span>
-                                <input type="text" className="form-control" value={DepartmentName} onChange={this.changeDepartmentName} />
+                                <input type="text" className="form-control"
+                                value={DepartmentName}
+                                onChange={this.changeDepartmentName}/>
                             </div>
-                            
-                            {DepartmentId ===0? <button type="button" className="btn btn-primary float-start">Create </button>:null}
-                            {DepartmentId !==0? <button type="button" className="btn btn-primary float-start">Update </button>:null}
+                          
+                            {DepartmentId==0?
+                            <button type="button"
+                            className="btn btn-primary float-start"
+                            onClick={()=>this.createClick()}
+                            >Create</button>
+                            :null}
+
+                            {DepartmentId!=0?
+                            <button type="button"
+                            className="btn btn-primary float-start"
+                            onClick={()=>this.updateClick()}
+                            >Update</button>
+                            :null}
 
                         </div>
 
--- WebAPI/UI/my-app/src/Employee.js
+++ WebAPI/UI/my-app/src/Employee.js
@@ -1,12 +1,21 @@
 import React,{Component} from "react";
-import {variables} from './Variables.js';
+import {variables} from './Variables'
 
-export class Employee extends Component{
+export class Employee  extends Component{
     constructor(props){
         super(props);
 
         this.state={
-            emps:[]
+            departments:[],
+            employees:[],
+            modalTitle:"",
+            EmployeeId:0,
+            EmployeeName:"",
+            Department:"",
+            DateOfJoining:"",
+            PhotoFileName:"anonymous.png",
+            PhotoPath:variables.PHOTO_URL
+
         }   
     }
     refreshList()
@@ -14,60 +23,272 @@ export class Employee extends Component{
         fetch(variables.API_URL+'employee')
         .then(response=>response.json())
         .then(data=>{
-            this.setState({emps:data});
+            this.setState({employees:data});
+        });
+
+        fetch(variables.API_URL+'department')
+        .then(response=>response.json())
+        .then(data=>{
+            this.setState({departments:data});
         });
     }
 
     componentDidMount(){
         this.refreshList();
     }
+    changeEmployeeName =(e)=>{
+        this.setState({EmployeeName:e.target.value});
+    }
+    changeDepartment =(e)=>{
+        this.setState({Department:e.target.value});
+    }
+    changeDateOfJoining =(e)=>{
+        this.setState({DateOfJoining:e.target.value});
+    }
+    addClick(){
+        this.setState({
+            modalTitle:"Add Employee",
+            EmployeeId:0,
+            EmployeeName:"",
+            Department:"",
+            DateOfJoining:"",
+            PhotoFileName:"anonymous.png"
+        });
+    }
+    editClick(emp){
+        this.setState({
+            modalTitle:"Edit Employee",
+            EmployeeId:emp.EmployeeId,
+            EmployeeName:emp.EmployeeName,
+            Department:emp.Department,
+            DateOfJoining:emp.DateOfJoining,
+            PhotoFileName:emp.PhotoFileName
+        });
+    }
 
+    createClick(){
+        fetch(variables.API_URL+'employee',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                EmployeeName:this.state.EmployeeName,
+                Department:this.state.Department,
+                DateOfJoining:this.state.DateOfJoining,
+                PhotoFileName:this.state.PhotoFileName
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+
+    updateClick(){
+        fetch(variables.API_URL+'employee',{
+            method:'PUT',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                EmployeeId:this.state.EmployeeId,
+                EmployeeName:this.state.EmployeeName,
+                Department:this.state.Department,
+                DateOfJoining:this.state.DateOfJoining,
+                PhotoFileName:this.state.PhotoFileName
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    deleteClick(id){
+        if(window.confirm('Are you sure?')){
+        fetch(variables.API_URL+'employee/'+id,{
+            method:'DELETE',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            }
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+        }
+    }
+    imageUpload=(e)=>{
+        e.preventDefault();
+
+        const formData=new FormData();
+        formData.append("file",e.target.files[0],e.target.files[0].name);
+
+        fetch(variables.API_URL+'employee/savefile',{
+            method:'POST',
+            body:formData
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            this.setState({PhotoFileName:data});
+        })
+    }
     render(){
         const {
-            emps
+            departments,
+            employees,
+            modalTitle,
+            EmployeeId,
+            EmployeeName,
+            Department,
+            DateOfJoining,
+            PhotoPath,
+            PhotoFileName
         }=this.state;
-        
+
         return(
         <div>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Employee Id</th>
-                        <th>Employee Name</th>
-                        <th>Department</th>
-                        <th>Date Of Joining</th>
-                        <th>Photo File Name</th>
-                        <th>Option</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {emps.map(emp=>
-                        <tr key={emp.EmployeeId}>
-                            <td>{emp.EmployeeId}</td>
-                            <td>{emp.EmployeeName}</td>
-                            <td>{emp.Department}</td>
-                            <td>{emp.DateOfJoining}</td>
-                            <td>{emp.PhotoFileName}</td>
-                            <td>
-                                <button type="button"
-                                className="btn btn-light mr-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                                    <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                                    </svg>
-                                </button>
 
-                                <button type="button"
-                                className="btn btn-light mr-1">
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                    </svg>
-                                </button>
-                            </td>
-                        </tr>
-                        )}
-                </tbody>
-            </table> 
+<button type="button"
+    className="btn btn-primary m-2 float-end"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+    onClick={()=>this.addClick()}>
+        Add Employee
+    </button>
+    <table className="table table-striped">
+    <thead>
+    <tr>
+        <th>
+            EmployeeId
+        </th>
+        <th>
+            EmployeeName
+        </th>
+        <th>
+            Department
+        </th>
+        <th>
+            DOJ
+        </th>
+        <th>
+            Options
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+        {employees.map(emp=>
+            <tr key={emp.EmployeeId}>
+                <td>{emp.EmployeeId}</td>
+                <td>{emp.EmployeeName}</td>
+                <td>{emp.Department}</td>
+                <td>{emp.DateOfJoining}</td>
+                <td>
+                <button type="button"
+                className="btn btn-light mr-1"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                onClick={()=>this.editClick(emp)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                    </svg>
+                </button>
+
+                <button type="button"
+                className="btn btn-light mr-1"
+                onClick={()=>this.deleteClick(emp.EmployeeId)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg>
+                </button>
+
+                </td>
+            </tr>
+            )}
+    </tbody>
+    </table>
+
+<div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
+<div className="modal-dialog modal-lg modal-dialog-centered">
+<div className="modal-content">
+   <div className="modal-header">
+       <h5 className="modal-title">{modalTitle}</h5>
+       <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"
+       ></button>
+   </div>
+
+   <div className="modal-body">
+    <div className="d-flex flex-row bd-highlight mb-3">
+     
+     <div className="p-2 w-50 bd-highlight">
+    
+        <div className="input-group mb-3">
+            <span className="input-group-text">Emp Name</span>
+            <input type="text" className="form-control"
+            value={EmployeeName}
+            onChange={this.changeEmployeeName}/>
+        </div>
+
+        <div className="input-group mb-3">
+            <span className="input-group-text">Department</span>
+            <select className="form-select"
+            onChange={this.changeDepartment}
+            value={Department}>
+                {departments.map(dep=><option key={dep.DepartmentId}>
+                    {dep.DepartmentName}
+                </option>)}
+            </select>
+        </div>
+
+        <div className="input-group mb-3">
+            <span className="input-group-text">DOJ</span>
+            <input type="date" className="form-control"
+            value={DateOfJoining}
+            onChange={this.changeDateOfJoining}/>
+        </div>
+
+
+     </div>
+     <div className="p-2 w-50 bd-highlight">
+         <img width="250px" height="250px"
+         src={PhotoPath+PhotoFileName}/>
+         <input className="m-2" type="file" onChange={this.imageUpload}/>
+     </div>
+    </div>
+
+    {EmployeeId==0?
+        <button type="button"
+        className="btn btn-primary float-start"
+        onClick={()=>this.createClick()}
+        >Create</button>
+        :null}
+
+        {EmployeeId!=0?
+        <button type="button"
+        className="btn btn-primary float-start"
+        onClick={()=>this.updateClick()}
+        >Update</button>
+        :null}
+   </div>
+
+</div>
+</div> 
+</div>
         </div>
         )       
     }
